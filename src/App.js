d19cbf6a2957253e32359c73dde3c30142019010
atsu0203 <atsu0203@gmail.com>
2024-02-17 16:43:08 +0900

    詳細データの取得

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
 import { toHaveFormValues } from "@testing-library/jest-dom/matchers";
 import "./App.css";
-import { getAllPokemon } from "./utils/pokemon";
-import { useEffect } from "react";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import { useEffect, useState } from "react";
+import Card from "./component/Card";
 
 function App() {
   const [loading, setloading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      loadPokemon(res.results);
+      setloading(false);
     };
     fetchPokemonData();
-    setloading(false);
   }, []);
-  {
-    loading ? (
-      <h1>ロード中</h1>
-    ) : (
-      <>
-        |<h1>ポケモンデータ取得済み</h1>
-      </>
-    );
-  }
 
-  return <div className="App"></div>;
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中</h1>
+      ) : (
+        <>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon}></Card>;
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
