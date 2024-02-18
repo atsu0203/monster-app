b0c3a78b06a74b1150205122f9b9b6af4bdad231
atsu0203 <atsu0203@gmail.com>
2024-02-19 00:06:21 +0900

    詳細情報取得

---
+++ src/component/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Card = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} alt="" />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div>タイプ</div>
+      {pokemon.types.map((type) => {
+        return (
+          <div>
+            <span className="typeName">{type.type.name}</span>
+          </div>
+        );
+      })}
+      ;
+    </div>
+  );
+};
+
+export default Card;
